bab7107f2459219a623e3b4c6724e71fb8e0a29f
Gloria Fulguera <gloriafulguera@gmail.com>
2025-05-31 16:54:39 -0300

    create components

---
+++ src/app/Components/character-details/character-details.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-character-details',
+  standalone: false,
+  templateUrl: './character-details.component.html',
+  styleUrl: './character-details.component.css'
+})
+export class CharacterDetailsComponent {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContainerComponent } from './Components/container/container.component';
+import { LocationsComponent } from './Components/locations/locations.component';
+import { CharactersComponent } from './Components/characters/characters.component';
+import { CharacterDetailsComponent } from './Components/character-details/character-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContainerComponent,
+    LocationsComponent,
+    CharactersComponent,
+    CharacterDetailsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [
     provideClientHydration(withEventReplay())
